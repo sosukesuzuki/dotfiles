a8d4458645e2194c0280f70aa331406b0cadc8df
Sosuke Suzuki <aosukeke@gmail.com>
2017-10-05 18:45:51 +0900

    config chunkwm

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -10,3 +10,6 @@ ln -sf ~/.ghq/github.com/sosukesuzuki/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/.ghq/github.com/sosukesuzuki/dotfiles/.zshenv ~/.zshenv
 ln -sf ~/.ghq/github.com/sosukesuzuki/dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/.ghq/github.com/sosukesuzuki/dotfiles/.tmux ~/.tmux
+ln -sf ~/.ghq/github.com/sosukesuzuki/dotfiles/.skhdrc ~/.skhdrc
+ln -sf ~/.ghq/github.com/sosukesuzuki/dotfiles/.chunkwmrc ~/.chunkwmrc
+ln -sf ~/.ghq/github.com/sosukesuzuki/dotfiles/.chunkwm_plugins ~/.chunkwm_plugins
