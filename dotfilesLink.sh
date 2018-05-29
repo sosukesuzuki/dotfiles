899d32f1c349a8d0a33c921af499f6a229290df8
Sosuke Suzuki <aosukeke@gmail.com>
2018-05-29 20:59:19 +0900

    remove .vim/ from link

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -3,7 +3,6 @@
 ln -sf ~/.ghq/github.com/sosukesuzuki/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/.ghq/github.com/sosukesuzuki/dotfiles/.spacemacs ~/.spacemacs
 ln -sf ~/.ghq/github.com/sosukesuzuki/dotfiles/.gitignore ~/.gitignore
-ln -sf ~/.ghq/github.com/sosukesuzuki/dotfiles/.vim ~/
 ln -sf ~/.ghq/github.com/sosukesuzuki/dotfiles/.emacs.d ~/.emacs.d
 ln -sf ~/.ghq/github.com/sosukesuzuki/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/.ghq/github.com/sosukesuzuki/dotfiles/.zshrc ~/.zshrc
